525feeeae81927cd53b42274527846fd3f5f6775
Sarvesh Patil <sarvesh@sarve.sh>
2025-01-30 23:44:16 +0530

    update nvim config

--- nvim/.config/nvim/lua/sarvesh/lazy.lua
+++ nvim/.config/nvim/lua/sarvesh/lazy.lua
@@ -14,7 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
         dependencies = {
             { "nvim-lua/plenary.nvim" },
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
